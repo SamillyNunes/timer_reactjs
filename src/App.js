8c344d0138ea5323264d0d13f5ce1c3ba6d0d12a
Samilly <samillynunes19@gmail.com>
2024-04-17 16:11:44 -0300

    Timer structure added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,56 @@ import React, { Component } from "react";
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      timerNumber: 0,
+      startBtnLabel: 'VAI'
+    }
+    this.timer=null;
+
+    this.startTimer = this.startTimer.bind(this);
+    this.cleanTimer = this.cleanTimer.bind(this);
+  }
+
+  startTimer(){
+    let state = this.state;
+
+    if(this.timer!==null){
+      clearInterval(this.timer);
+      this.timer=null;
+      state.startBtnLabel='VAI';
+      
+      this.setState(state);
+    } else {
+      state.startBtnLabel='PAUSAR';
+      this.timer = setInterval(() => {
+        state.timerNumber += 0.1;
+        this.setState(state);
+      }, 100);
+    }
+  }
+
+  cleanTimer(){
+    if(this.timer!==null){
+      clearInterval(this.timer);
+      this.timer=null;
+    }
+
+    let state = this.state;
+    state.timerNumber = 0;
+    state.startBtnLabel='VAI';
+    this.setState(state);
+  }
+
   render(){
     return (
       <div className="container">
         <img src={require('./assets/cronometro.png')} alt="'Cronometro" className="img" />
-        <a className="timer">0.0</a>
+        <a className="timer">{this.state.timerNumber.toFixed(1)}</a>
         <div className="areaBtn">
-          <a className="botao">VAI</a>
-          <a className="botao">ZERAR</a>
+          <a className="botao" onClick={this.startTimer} > {this.state.startBtnLabel} </a>
+          <a className="botao" onClick={this.cleanTimer} >ZERAR</a>
         </div>
       </div>
     );
