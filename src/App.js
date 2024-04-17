e055ca8b882ebcf11e82ea081d1850b9223451b4
Samilly <samillynunes19@gmail.com>
2024-04-17 15:56:26 -0300

    Structure and style added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="container">
+        <img src={require('./assets/cronometro.png')} alt="'Cronometro" className="img" />
+        <a className="timer">0.0</a>
+        <div className="areaBtn">
+          <a className="botao">VAI</a>
+          <a className="botao">ZERAR</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
